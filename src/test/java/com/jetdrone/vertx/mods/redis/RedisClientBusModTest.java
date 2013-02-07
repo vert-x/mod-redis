5a6b0a9dd54583423d3ec7f8ea56d630e965910c
Paulo Lopes <plopes@roughcookie.com>
2013-02-07 11:36:51 +0100

    enable running unit tests from maven

---
+++ src/test/java/com/jetdrone/vertx/mods/redis/RedisClientBusModTest.java
@@ -0,0 +1,23 @@
+package com.jetdrone.vertx.mods.redis;
+
+import org.junit.Test;
+import org.vertx.java.testframework.TestBase;
+
+public class RedisClientBusModTest extends TestBase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        startApp(TestClient.class.getName());
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @Test
+    public void testPersistor() throws Exception {
+        startTest(getMethodName());
+    }
+}
---
+++ src/test/java/com/jetdrone/vertx/mods/redis/TestClient.java
@@ -0,0 +1,65 @@
+package com.jetdrone.vertx.mods.redis;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.testframework.TestClientBase;
+
+public class TestClient extends TestClientBase {
+
+    private final String address = "test.my_redisclient";
+    private EventBus eb;
+
+    @Override
+    public void start() {
+        super.start();
+        eb = vertx.eventBus();
+        JsonObject config = new JsonObject();
+
+        config.putString("address", address);
+        config.putString("host", "localhost");
+        config.putNumber("port", 6379);
+
+        container.deployModule("vertx.mods.redis-DEV", config, 1, new Handler<String>() {
+            public void handle(String res) {
+                tu.appReady();
+            }
+        });
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+    }
+
+    public void testPersistor() throws Exception {
+
+        final String random = Long.toHexString(System.nanoTime());
+
+        JsonObject message = new JsonObject();
+        message.putString("command", "set");
+        message.putString("key", "myKey");
+        message.putString("value", random);
+
+        eb.send(address, message, new Handler<Message<JsonObject>>() {
+            @Override
+            public void handle(Message<JsonObject> reply) {
+                tu.azzert("ok".equals(reply.body.getString("status")));
+
+                JsonObject message = new JsonObject();
+                message.putString("command", "get");
+                message.putString("key", "myKey");
+
+                eb.send(address, message, new Handler<Message<JsonObject>>() {
+                    @Override
+                    public void handle(Message<JsonObject> reply) {
+                        tu.azzert("ok".equals(reply.body.getString("status")));
+                        tu.azzert(random.equals(reply.body.getString("value")));
+                        tu.testComplete();
+                    }
+                });
+            }
+        });
+    }
+}
